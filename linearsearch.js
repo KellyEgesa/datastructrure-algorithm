7f45446de9639a0dbfcf97225e146c360187865f
KellyEgesa <kelly.egesa@gmail.com>
2021-04-01 18:33:01 +0300

    Corrected typo errors

--- linearsearch.js
+++ linearsearch.js
@@ -1,8 +1,7 @@
 function linearSearch(arr, val) {
-  for (let index = 0; index < array.length; index++) {
-    if (arr[index] === val) {
-      return index;
-    }
+  for (let index = 0; index < arr.length; index++) {
+    if (arr[index] === val) return index;
   }
   return -1;
 }
+console.log(linearSearch([34, 56, 1, 2, 3], 1));
